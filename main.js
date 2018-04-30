82c310b6074a537299e78e2e9b11d2d36804d302
EduBQr <eduardobq3@gmail.com>
2018-04-30 12:08:27 -0300

    + devtools, +add itens

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
-// Listen for app to be ready
+// Verificar se o aplicativo está pronto
 app.on('ready', function(){
     //cria nova janela
     mainWindow = new BrowserWindow({});
@@ -16,27 +17,57 @@ app.on('ready', function(){
         protocol:'file:',
         slashes: true
     }));
+    // Sair da subjanelas quando fechar a janela principal
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
 
-    // Build menu from template
+    // Criar menu template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    // Insert menu
+    // Inserir menu
     Menu.setApplicationMenu(mainMenu);
 });
 
 
-// create menu template
+// Criar addWindow
+function createAddWindow(){
+    // cria nova janela
+    addWindow = new BrowserWindow({
+        width:300,
+        height:200,
+        title:'Add Item a Lista de Compras'
+    });
+    // carrega html na janela
+    addWindow.loadURL(url.format({
+        pathname:path.join(__dirname, 'addWindow.html'),
+        protocol:'file:',
+        slashes: true
+    }));
+    //
+    addWindow.on('close', function(){
+        addWindow = null;
+    });
+
+}
+
+// menu template
 const mainMenuTemplate = [
     {
         label:'File',
         submenu:[
             {
-                label: 'Add Item'
+                label: 'Add Item',
+                click(){
+                    createAddWindow();
+                }
             },
             {
                 label: 'Clear Items'
             },
             {
                 label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' :
+                'Ctrl+Q',
                 click(){
                     app.quit();
                 }
@@ -44,3 +75,28 @@ const mainMenuTemplate = [
         ]
     }
 ];
+// if mac, add objeto vazio no menu (corrige nome electron ao inves de file no menu)
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({}); // unshift -> adiciona {} no inicio do menu
+}
+// Add no menu principal -> Ferramentas do Desenvolvedor
+if(process.env.NODE_ENV != 'production'){
+    mainMenuTemplate.push({
+        label: 'Dev Tools',
+        submenu:[
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' :
+                'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+
+        ]
+    })
+
+}
